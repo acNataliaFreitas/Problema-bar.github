be77b204152d0a8ce861c8c863bf9480ae6a4c19
Natália Freitas <natyff1@hotmail.com>
2020-05-07 18:33:16 -0300

    Relatorio gerado

--- src/application/bar.java
+++ src/application/bar.java
@@ -11,6 +11,8 @@ public class bar {
 		
 		char sexo;
 		int cerveja,espetinho,refri;
+		double ingresso,consumo,cover,total;
+		
 		System.out.println("Sexo: ");
 		sexo=sc.next().charAt(0);
 		System.out.println("Quantidade de Cervejas: ");
@@ -20,11 +22,38 @@ public class bar {
 		System.out.println("Quantidade de espetinhos: ");
 		espetinho = sc.nextInt();
 		
+		// CALCULOS
+		if(sexo=='F') {
+			ingresso = 8.0;
+		}
+		else {
+			ingresso = 10.0;
+		}
 		
+		consumo = cerveja * 5.0 + refri * 3.0 + espetinho * 7.0;
 		
+		if(consumo>30) {
+			cover = 0.0;
+		}
+		else{
+			cover = 4.0;
+		}
+		total=ingresso + consumo + cover;
 		
+		// RELATORIO
 		
-		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if(cover==0.0) {
+			System.out.println("Isento do Couvert");
+		}
+		else {
+			System.out.printf("Couvert = R$ %.2f%n", cover);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 		sc.close();
 	}
