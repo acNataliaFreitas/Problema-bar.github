5f09a393c320a974ea366b91dcdd4e919b79fe35
Natália Freitas <natyff1@hotmail.com>
2020-05-07 18:13:35 -0300

    Leitura dos dados

--- src/application/bar.java
+++ src/application/bar.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
 public class bar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		char sexo;
+		int cerveja,espetinho,refri;
+		System.out.println("Sexo: ");
+		sexo=sc.next().charAt(0);
+		System.out.println("Quantidade de Cervejas: ");
+		cerveja = sc.nextInt();
+		System.out.println("Quantidade de Refrigerantes: ");
+		refri = sc.nextInt();
+		System.out.println("Quantidade de espetinhos: ");
+		espetinho = sc.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
